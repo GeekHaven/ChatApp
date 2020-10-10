6e8e8a35008604028681296fddad4092239a75b3
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-10-10 06:51:20 +0530

    Add Navigation Drawer in Main Activity #2

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setSupportActionBar(mToolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void initView(){
+        mDrawerLayout=findViewById(R.id.drawer_main);
+        mToolbar=findViewById(R.id.toolbar);
+        mNavigationView=findViewById(R.id.nav_view);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.my_profile_menu_item:
+                //temporary toast
+                Toast.makeText(this,R.string.my_profile,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chat_list_menu_item:
+                //temporary toast
+                Toast.makeText(this,R.string.chat_list,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.find_friends_menu_item:
+                //temporary toast
+                Toast.makeText(this,R.string.find_friends,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings_menu_item:
+                //temporary toast
+                Toast.makeText(this,R.string.settings,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout_menu_item:
+                //temporary toast
+                Toast.makeText(this,R.string.logout,Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
