766315b0bd4e1b99bc236e21f111bcd62d102ee2
Pavika <54035413+pavikachaudhary@users.noreply.github.com>
2020-10-13 14:21:10 +0530

    Trying to solve conflicts

--- app/src/main/java/com/example/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -5,39 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import android.util.Log;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import java.util.regex.Pattern;
-
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
     private GoogleSignInClient mGoogleSignInClient;
     private Button GoogleSignInBtn;
+    private FirebaseAuth mAuth;
+
     EditText email;
     EditText password;
     Button emailsignin;
-    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onStart() {
@@ -54,12 +53,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth=FirebaseAuth.getInstance();
+        GoogleSignInBtn=findViewById(R.id.google_sign_in_btn);
+        createRequest();
+        GoogleSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+
 
         email=findViewById(R.id.editTextTextPersonName);
         password=findViewById(R.id.editTextTextPassword);
         emailsignin=findViewById(R.id.button2);
-
-        mAuth = FirebaseAuth.getInstance();
+        
 
         emailsignin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,19 +109,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-        GoogleSignInBtn=findViewById(R.id.google_sign_in_btn);
-        createRequest();
-        GoogleSignInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-
-
     }
-
-
 
     private void createRequest() {
         // Configure Google Sign In
