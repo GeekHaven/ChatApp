67d476b83980a387def7e2528f8ebee9f0c4ce00
deepakgupta124 <iec2019075@iiita.ac.in>
2020-10-25 15:07:41 +0530

    added new welcome screen with 2 sign in buttons

---
+++ app/src/main/java/com/example/chatapp/WelcomeScreen.java
@@ -0,0 +1,34 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        Button signInWithEmail = findViewById(R.id.sign_in_with_email);
+        signInWithEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
+        Button signInWithGoogle = findViewById(R.id.sign_in_with_google);
+        signInWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
+    }
+}
